c3b0dfa9f3c3a53522c5d6d7abb9f9f60ed77ae3
Aayush Agnihotri <your_github_email@example.com>
2025-11-04 18:02:20 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["https://aayushagnihotri35.github.io"], // your GitHub Pages domain
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 // Routes
 app.use("/api/auth", authRoutes);
